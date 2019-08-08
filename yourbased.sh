6a48cc888fcd305d4a3571e9842e75c71968a980
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 14:44:25 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y tzdata
+apt-get update && apt-get install -y tzdata  libpq-dev
 gem install bundler -v 2.0.1
 # install
 bundle install --jobs=3 --retry=3
